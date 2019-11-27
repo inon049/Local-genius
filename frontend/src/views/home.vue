a786b799b3ba01266ae68b411a63289388ab81a8
Dror Uzi <dror.uzi5@gmail.com>
2019-11-27 18:07:01 +0200

    still testing branches

--- frontend/src/views/home.vue
+++ frontend/src/views/home.vue
@@ -1,15 +1,6 @@
 <template>
   <div class="home">
     <h1>Homepage</h1>
-<<<<<<< HEAD
-    <h1>Dror</h1>
-  <h2>aaaa</h2>
-  <h3>Dror</h3>
-  <h1>l;j;lskjdf</h1>
-=======
-    <h2>testing</h2>
->>>>>>> dror
-  </div>
 </template>
 
 <script>
@@ -21,7 +12,6 @@ export default {
   async created(){
     var users = await httpService.get('user')
     console.log(users);
-    
   }
 }
 </script>
