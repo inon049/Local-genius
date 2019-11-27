dc95fe502909bd16a89c3f40901abbb28048d201
Inon Amsalem <inon049@gmail.com>
2019-11-27 21:18:09 +0200

    `

--- frontend/src/services/city.service.js
+++ frontend/src/services/city.service.js
@@ -1,74 +1,9 @@
 'use strict';
 
-export default{
-    query
+export default {
+    query,
+    getById,
 }
-
-var gCities = [
-    {
-        id: 101,
-        name: 'New-York',
-        imgUrl: 'https://cdn.getyourguide.com/img/tour_img-1667715-146.jpg'
-    },
-    {
-        id: 102,
-        name: 'Moscow',
-        imgUrl:'https://cdn.getyourguide.com/img/tour_img-1667715-146.jpg'
-    },
-    {
-        id: 103,
-        name: 'Paris',
-        imgUrl: 'https://cdn.getyourguide.com/img/tour_img-1667715-146.jpg'
-    },
-    {
-        id: 104,
-        name: 'Sydney',
-        imgUrl: 'https://cdn.getyourguide.com/img/tour_img-1667715-146.jpg'
-    },
-    {
-        id: 106,
-        name: 'Mexico-City',
-        imgUrl: 'https://cdn.getyourguide.com/img/tour_img-1667715-146.jpg'
-    },
-    {
-        id: 107,
-        name: 'Athens',
-        imgUrl: 'https://cdn.getyourguide.com/img/tour_img-1667715-146.jpg'
-    },
-    {
-        id: 108,
-        name: 'Seoul',
-        imgUrl: 'https://cdn.getyourguide.com/img/tour_img-1667715-146.jpg'
-        _id: "101",
-        name: 'New-York'
-    },
-    {
-        _id: "102",
-        name: 'Moscow'
-    },
-    {
-        _id: "103",
-        name: 'Paris'
-    },
-    {
-        _id: "104",
-        name: 'Sydney'
-    },
-    {
-        _id: "106",
-        name: 'Mexico-City'
-    },
-    {
-        _id: "107",
-        name: 'Athens'
-    },
-    {
-        _id: "108",
-        name: 'Seoul'
-    },
-]
-
-
 async function query() {
     return gCities
 }
@@ -77,7 +12,42 @@ async function getById(cityId) {
     return city
 }
 
-export default {
-    query,
-    getById,
-}
+
+var gCities = [
+    {
+        _id: '101',
+        name: 'New-York',
+        imgUrl: 'https://cdn.getyourguide.com/img/tour_img-1667715-146.jpg'
+    },
+    {
+        _id: '102',
+        name: 'Moscow',
+        imgUrl:'https://cdn.getyourguide.com/img/tour_img-1667715-146.jpg'
+    },
+    {
+        _id: '103',
+        name: 'Paris',
+        imgUrl: 'https://cdn.getyourguide.com/img/tour_img-1667715-146.jpg'
+    },
+    {
+        _id: '104',
+        name: 'Sydney',
+        imgUrl: 'https://cdn.getyourguide.com/img/tour_img-1667715-146.jpg'
+    },
+    {
+        _id: '106',
+        name: 'Mexico-City',
+        imgUrl: 'https://cdn.getyourguide.com/img/tour_img-1667715-146.jpg'
+    },
+    {
+        _id: '107',
+        name: 'Athens',
+        imgUrl: 'https://cdn.getyourguide.com/img/tour_img-1667715-146.jpg'
+    },
+    {
+        _id: '108',
+        name: 'Seoul',
+        imgUrl: 'https://cdn.getyourguide.com/img/tour_img-1667715-146.jpg',
+    }]
+
+
