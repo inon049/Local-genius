f9b10904bf28bee4f4cbcb566a07288523cc6a51
Ori Shemla <orish1001@gmail.com>
2019-11-27 19:06:38 +0200

    sd

--- frontend/src/views/city-guide.vue
+++ frontend/src/views/city-guide.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="home">
-    <h2>inon</h2>
-    <h1>Homepage</h1>
+    <h2>city Guides</h2>
 
   </div>
 </template>
 
 <script>
+
 export default {
   name: "home",
   components: {},
