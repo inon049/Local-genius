8da5624aa0aa539e3ddac802882dd8834b5ded08
Inon Amsalem <inon049@gmail.com>
2019-12-01 22:16:13 +0200

    s

--- frontend/src/components/profile/booking-preview.vue
+++ frontend/src/components/profile/booking-preview.vue
@@ -1,22 +1,17 @@
 <template>
   <section class="booking-preview">
-      
+      <pre>{{booking}}</pre>
   </section>
 </template>
 
 <script>
 export default {
-
+props:{
+  booking:Object
+}
 }
 
-// {
-//       at: "2019-12-01T22:00:00.000Z",
-//       attendees: "3",
-//       byUserId: "5de3ed727fe0b526944c41e8",
-//       createdAt: 1575219772745,
-//       price: 9999999,
-//       toGuideId: "5de3ed727fe0b526944c41e2"
-//     }
+
 </script>
 
 <style>
