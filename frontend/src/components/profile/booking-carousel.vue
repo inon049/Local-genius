fe3328e5610bb75636e37d694f930b011fda0717
Inon Amsalem <inon049@gmail.com>
2019-12-02 11:21:47 +0200

    123

--- frontend/src/components/profile/booking-carousel.vue
+++ frontend/src/components/profile/booking-carousel.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="booking-carousel">
-    <carousel :perPage="1" :perPageCustom="[[1100, 2], [1400, 4]]">
+    <carousel :perPage="1" :perPageCustom="[[1100, 2], [1400, 3]]">
       <slide v-for="booking in bookings" :key="booking.id">
         <booking-preview  :booking="booking"></booking-preview>
       </slide>
