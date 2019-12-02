90e9baaaf4314e515aed6a6d0ee3154cca72cfc7
Ori Shemla <orish1001@gmail.com>
2019-12-02 11:26:16 +0200

    img for booking

--- frontend/src/services/user.service.js
+++ frontend/src/services/user.service.js
@@ -2,19 +2,27 @@
 
 import httpService from '@/services/http.service'
 
-async function query(){
-    const users = await httpService.get('user')
+const USER_URL = 'user/'
+
+async function query() {
+    const users = await httpService.get(USER_URL)
     return users
 }
 
-async function getGuideById(_id){    
-    const guide = await httpService.get(`user/${_id}`)
+async function getGuideById(_id) {
+    const guide = await httpService.get(USER_URL + _id)
     return guide
+}
+
+async function addUser(user) {
+    const newUser = await httpService.post(USER_URL, user)
+    return newUser
 }
 
 
 
 export default {
     query,
-    getGuideById
+    getGuideById,
+    addUser
 }
