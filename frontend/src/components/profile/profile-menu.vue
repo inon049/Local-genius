078883a597f09a54f7ec82b0d1031e9392cafdad
Inon Amsalem <inon049@gmail.com>
2019-12-03 16:17:49 +0200

    123

--- frontend/src/components/profile/profile-menu.vue
+++ frontend/src/components/profile/profile-menu.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav class="profile-menu flex ">
+  <nav class="profile-menu flex">
       <router-link to="/profile/summary">Summary</router-link>
       <router-link to="/profile/messenger">Messeges</router-link>
   </nav>
