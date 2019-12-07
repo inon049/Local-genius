de7becd4fbc059cd96205489f6b25566b8dd1d13
Ori Shemla <orish1001@gmail.com>
2019-12-07 11:36:57 +0200

    cd

--- frontend/src/app.vue
+++ frontend/src/app.vue
@@ -1,38 +1,38 @@
 <template>
   <div id="app">
-   <main-header></main-header>
+    <main-header></main-header>
     <section class="main">
       <router-view />
-          <a class="scroll-up" href="#" v-scroll-to="'#app'"><img class="arrow-up" src="../src/assets/img/arrow-up.png"></a>
+      <a class="scroll-up" href="#" v-scroll-to="'#app'">
+        <img class="arrow-up" src="../src/assets/img/arrow-up.png" />
+      </a>
     </section>
     <mobile-nav></mobile-nav>
   </div>
 </template>
 
 <script>
-import mainHeader from './components/main-header'
-import mobileNav from './components/mobile-nav'
+import mainHeader from "./components/main-header";
+import mobileNav from "./components/mobile-nav";
 export default {
-  data(){
-    return{
-      isScroll : false
-    }
+  data() {
+    return {
+      isScroll: false
+    };
   },
- async created() {
-   if(this.$store.getters.loggedInUser) this.$store.dispatch({type:'createUserSocket'})
-     await this.$store.dispatch({ type: "loadUsers" });
-     await this.$store.dispatch({ type: "loadCities" });
-    window.addEventListener('scroll', this.handleScroll);
-
+  async created() {
+    if (this.$store.getters.loggedInUser)
+      this.$store.dispatch({ type: "createUserSocket" });
+    await this.$store.dispatch({ type: "loadUsers" });
+    await this.$store.dispatch({ type: "loadCities" });
+    window.addEventListener("scroll", this.handleScroll);
   },
-  components:{
+  components: {
     mainHeader,
     mobileNav
   },
-   destroyed () {
-    window.removeEventListener('scroll', this.handleScroll);
+  destroyed() {
+    window.removeEventListener("scroll", this.handleScroll);
   }
 };
-
-
 </script>
